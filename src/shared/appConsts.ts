82ad38335f06b1bc5da931393beacf648240e9bb
levtrilev <levtrishankov@yandex.ru>
2022-02-28 21:12:50 +0300

    https connection to backend added

--- src/shared/appConsts.ts
+++ src/shared/appConsts.ts
@@ -1,3 +1,4 @@
-export const DOMAIN = "http://95.165.6.202:3000";
+// export const DOMAIN = "http://95.165.6.202:3000";
+export const DOMAIN = "https://95.165.6.202";
 export const tenantId = 1;
 export const sectionId = 1;
